20ecd4c6f96fe2653a00850c88d6ca92c6801178
Paulo Lima <paulolima@uepa.br>
2023-12-03 13:40:04 -0300

    ok

--- ProgramasPython/TestesPython.py
+++ ProgramasPython/TestesPython.py
@@ -1,15 +1,17 @@
-
-from pyvis.network import Network
-
-
-# Instancia o grafo G (vazio, o parâmetro notebook=True é para visualizar no Jupyter Notebook)
-G = Network()
-G.add_node("Singapore")
-G.add_node("San Francisco")
-G.add_node("Tokyo")
-# add_nodes adiciona vários nós a partir de uma lista
-G.add_nodes(["Riga", "Copenhagen"],
-              color=['lightgreen', 'yellow'])
-
-
-G.show_buttons(filter_=['physics'])
+import jupyter_to_medium as jtm
+jtm.publish('IntroducaoAnalisedeGrafoscomPython.ipynb',
+            integration_token="22f375256a2403556549a099732b7852ae8c2a3e2df355326d49d5b2f739b0ee0",
+            pub_name="xxxxx",
+            title="xxxx",
+            tags="xxxx",
+            publish_status='draft',
+            notify_followers=False,
+            license='all-rights-reserved',
+            canonical_url=None,
+            chrome_path=None,
+            save_markdown=False,
+            table_conversion='chrome',
+            #gistify=False,
+            #gist_threshold=5,
+            #public_gists=True
+            )

7795f63df7b8b20261052cd8940eaf9a1996c867
Paulo Lima <paulolima@uepa.br>
2024-10-12 13:05:16 -0300

    ok

---
+++ Testes/testePython.py
@@ -0,0 +1,11 @@
+# Programa para ler duas variáveis, calcular e escrever a média
+
+# Leitura das variáveis
+var1 = float(input("Digite o valor da primeira variável: "))
+var2 = float(input("Digite o valor da segunda variável: "))
+
+# Cálculo da média
+media = (var1 + var2) / 2
+
+# Exibição do resultado
+print(f"A média dos valores é: {media}")

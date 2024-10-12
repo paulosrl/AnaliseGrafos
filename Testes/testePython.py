145d355025a59eb93844d23235517357946739fb
Paulo Lima <paulolima@uepa.br>
2024-10-12 13:15:27 -0300

    ok

--- Testes/testePython.py
+++ Testes/testePython.py
@@ -3,9 +3,7 @@
 # Leitura das variáveis
 var1 = float(input("Digite o valor da primeira variável: "))
 var2 = float(input("Digite o valor da segunda variável: "))
-
 # Cálculo da média
 media = (var1 + var2) / 2
-
 # Exibição do resultado
 print(f"A média dos valores é: {media}")
